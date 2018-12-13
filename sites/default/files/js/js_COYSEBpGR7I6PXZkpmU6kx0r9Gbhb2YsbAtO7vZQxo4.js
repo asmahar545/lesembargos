14198d019a2e32e40b6462e3a9914f75bb2b72c2
Manirakiza <mj.manirakiza@grip.org>
2018-12-13 17:50:49 +0100

    commit 4

---
+++ sites/default/files/js/js_COYSEBpGR7I6PXZkpmU6kx0r9Gbhb2YsbAtO7vZQxo4.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Javascript file for the FooTable module.
+ */
+
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.footable = {
+    attach: function (context) {
+      $('.footable', context).footable();
+    }
+  };
+}(jQuery));
+;
+;
---
+++ sites/default/files/php/twig/5c127f71be040_image-formatter--node--fi_-D_hrBawuDi4jtz7wrZROGlQE/ax8zkADhrnrevHtjHEJjrWIuVfeODwroPoWscIR9lEk.php
@@ -0,0 +1,87 @@
+<?php
+
+/* themes/newsplus_lite/templates/image-formatter--node--field-image.html.twig */
+class __TwigTemplate_97d95854ed9fdc528de740f86ce15245bc933f788f315821fa45e47861a5e246 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 15);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 15
+        if (($context["url"] ?? null)) {
+            // line 16
+            echo "  <a class=\"image-popup overlayed\" href=\"";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["url"] ?? null), "html", null, true));
+            echo "\">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["image"] ?? null), "html", null, true));
+            echo "<span class=\"overlay\"><i class=\"fa fa-plus\"></i></span></a>
+";
+        } else {
+            // line 18
+            echo "  ";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["image"] ?? null), "html", null, true));
+            echo "
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/newsplus_lite/templates/image-formatter--node--field-image.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  53 => 18,  45 => 16,  43 => 15,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/newsplus_lite/templates/image-formatter--node--field-image.html.twig", "C:\\wamp64\\www\\drupal-8.6.3\\themes\\newsplus_lite\\templates\\image-formatter--node--field-image.html.twig");
+    }
+}
